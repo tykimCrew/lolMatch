591458ef5bf0c5e3df14cc2d501905ca8c63b8d8
tykimCrew <cleansun9099@naver.com>
2020-12-31 01:27:12 +0900

    search Page  cnt up

--- src/containers/BoardUrlInput.js
+++ src/containers/BoardUrlInput.js
@@ -19,96 +19,103 @@ function getCmtListAjax(boardUrl, dispatch) {
    * urlArr[3] = user_id
    * urlArr[5] = 방송국게시판 순번
    */
-  var url = 'https://bjapi.afreecatv.com/api/' + urlArr[3] + '/title/' + urlArr[5] + '/comment?page=1&orderby=like_cnt';
-
+  var url = '';
+  var targetPageArr = [1, 2, 3];
   /**
    * 덧글리스트 API 시작
    */
-  $.ajax({
-    url: url,
-    context: this //context : this => .done()에서 this를 사용하기위해
-  }).done(json => {
-    var result = []; //최종 memberList 담을 변수 선언
+  var filterdCmtList = [];
+  var result = []; //최종 memberList 담을 변수 선언
+
+  /**
+   * 3페이지 리스트 가져온다.
+   */
+  for (var i in targetPageArr) {
+    url = 'https://bjapi.afreecatv.com/api/' + urlArr[3] + '/title/' + urlArr[5] + '/comment?page=' + i + '&orderby=like_cnt';
+    $.ajax({
+      url: url,
+      context: this, //context : this => .done()에서 this를 사용하기위해
+      async: false
+    }).done(json => {
+      /**
+       * 댓글을 중복으로달았을시 중복제거
+       */
+      json.data.forEach((item) => {
+        if (!filterdCmtList.find(cmt => cmt.user_id === item.user_id)) {
+          filterdCmtList.push(item);
+        }
+      });
+    });
+  }
+
+  /**
+       * 가져온 JSON데이터로 내가원하는데이터만 뽑기.
+       */
+  filterdCmtList.forEach((item) => {
 
     /**
-     * 댓글을 중복으로달았을시 중복제거
+     * item.comment  = "원딜,서폿/골드4"
      */
-    var filterdCmtList = [];
-    json.data.forEach((item) => {
-      if (!filterdCmtList.find(cmt => cmt.user_id === item.user_id)) {
-        filterdCmtList.push(item);
-      }
+    var commentDataArr = item.comment.split('/');
+    if (!commentDataArr || commentDataArr.length < 2) return;
+
+    /**
+     * 덧글로 입력한 포지션을 분석해서 코드에필요한 포지션데이터로 가공
+     * ../script/PositionTierData 에 정의
+     */
+    var myPositions = commentDataArr[0].split(',');
+    var posiResult = [];
+    myPositions.forEach((myPosition) => {
+      var posiJson = pattern_kor.test(myPosition) ? posiKorJson : posiEngJson;
+      var resultKey = '';
+
+      Object.getOwnPropertyNames(posiJson).forEach((key) => {
+        if (resultKey) return false;
+
+        posiJson[key].forEach((posiValue) => {
+          if (posiValue === myPosition.toLowerCase()) { resultKey = key; return false; }
+        });
+      });
+
+      posiResult.push(resultKey);
     });
 
     /**
-     * 가져온 JSON데이터로 내가원하는데이터만 뽑기.
+     * 덧글로 입력한 티어을 분석해서 코드에필요한 티어데이터로 가공
+     * ../script/PositionTierData 에 정의
      */
-    filterdCmtList.forEach((item) => {
+    var myTier = commentDataArr[1];
+    var tierResult = '';
+    var tierJson = pattern_kor.test(myTier) ? tierKorJson : tierEngJson;
 
-      /**
-       * item.comment  = "원딜,서폿/골드4"
-       */
-      var commentDataArr = item.comment.split('/');
-      if (!commentDataArr || commentDataArr.length < 2) return;
+    Object.getOwnPropertyNames(tierJson).forEach((key) => {
+      if (tierResult) return false;
 
-      /**
-       * 덧글로 입력한 포지션을 분석해서 코드에필요한 포지션데이터로 가공
-       * ../script/PositionTierData 에 정의
-       */
-      var myPositions = commentDataArr[0].split(',');
-      var posiResult = [];
-      myPositions.forEach((myPosition) => {
-        var posiJson = pattern_kor.test(myPosition) ? posiKorJson : posiEngJson;
-        var resultKey = '';
-
-        Object.getOwnPropertyNames(posiJson).forEach((key) => {
-          if (resultKey) return false;
-
-          posiJson[key].forEach((posiValue) => {
-            if (posiValue === myPosition.toLowerCase()) { resultKey = key; return false; }
-          });
-        });
-
-        posiResult.push(resultKey);
-      });
-
-      /**
-       * 덧글로 입력한 티어을 분석해서 코드에필요한 티어데이터로 가공
-       * ../script/PositionTierData 에 정의
-       */
-      var myTier = commentDataArr[1];
-      var tierResult = '';
-      var tierJson = pattern_kor.test(myTier) ? tierKorJson : tierEngJson;
-
-      Object.getOwnPropertyNames(tierJson).forEach((key) => {
-        if (tierResult) return false;
-
-        tierJson[key].forEach((tierValue) => {
-          if (myTier.toLowerCase().indexOf(tierValue) > -1) { tierResult = key; return false; }
-        });
-      });
-
-      /**
-       * 가공한 데이터를 최종결과변수에 넣기
-       */
-      result.push({
-        userId: item.user_id,
-        userNick: item.user_nick,
-        profileImage: item.profile_image,
-        comment: item.comment,
-        likeCnt: item.like_cnt,
-        myPosition: posiResult,
-        myTier: tierResult,
-        side: 'center', //side 초기값
-        position: '' //position 초기값
+      tierJson[key].forEach((tierValue) => {
+        if (myTier.toLowerCase().indexOf(tierValue) > -1) { tierResult = key; return false; }
       });
     });
 
     /**
-     * store에 dispatch실행
+     * 가공한 데이터를 최종결과변수에 넣기
      */
-    dispatch({ type: 'ajax_getCmtList', memberList: result });
+    result.push({
+      userId: item.user_id,
+      userNick: item.user_nick,
+      profileImage: item.profile_image,
+      comment: item.comment,
+      likeCnt: item.like_cnt,
+      myPosition: posiResult,
+      myTier: tierResult,
+      side: 'center', //side 초기값
+      position: '' //position 초기값
+    });
   });
+
+  /**
+   * store에 dispatch실행
+   */
+  dispatch({ type: 'ajax_getCmtList', memberList: result });
 }
 
 function mapStateToProps(reduxState) {
